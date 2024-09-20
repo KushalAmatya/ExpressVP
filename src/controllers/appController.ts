3adaa58912d09ed07b9d83210480214907edaf76
Kushal Amatya <kushalamatya413@gmail.com>
2024-09-20 10:46:22 +0545

    (AppRouter):Added multer file upload to route

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -14,4 +14,24 @@ const getTodos = async (req: Request, res: Response) => {
   res.json(todos);
 };
 
-export { addTodo, getTodos };
+const deleteTodo = async (req: Request, res: Response) => {
+  await appModel.findByIdAndDelete(req.params.id);
+  res.json({ message: "Todo deleted successfully" });
+};
+
+const updateTodo = async (req: Request, res: Response) => {
+  if (!req.body.title || !req.body.description) {
+    return res.status(400).json({ message: "Please provide all details" });
+  }
+
+  if (!req.query.id) {
+    return res.status(400).json({ message: "Please provide id" });
+  }
+
+  await appModel.findByIdAndUpdate(req.query.id, {
+    title: req.body.title,
+    description: req.body.description,
+  });
+  res.json({ message: "Todo updated successfully" });
+};
+export { addTodo, getTodos, deleteTodo, updateTodo };
--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import { isAuth } from "../middleware/authValidationMiddleware";
-import { addTodo, getTodos } from "../controllers/appController";
-const appRouter = express.Router();
+import multer from "multer";
+import {
+  addTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/appController";
 
-appRouter.post("/add", isAuth, addTodo);
+const appRouter = express.Router();
+const upload = multer({ dest: "uploads/" });
+appRouter.post("/add", upload.single("image"), isAuth, addTodo);
 appRouter.get("/todos", isAuth, getTodos);
+appRouter.delete("/delete/:id", isAuth, deleteTodo);
+appRouter.put("/update", isAuth, updateTodo);
 export default appRouter;
